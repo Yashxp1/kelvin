30db56e0c1d7e09c34bc67370424bc7c083cce91
Yash Raj <kyashraj991@gmail.com>
2025-12-02 14:05:00 +0530

    feat: Implement initial login and registration pages with new Card and Label UI components and updated dependencies.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import React from 'react';
 
 const page = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-blue-900">
-      <h1>This is the landing page - to be done later!!!!!!!!</h1>
+    <div className="flex justify-center items-center h-screen bg-blue-800">
+      <h1>This is the landing page - to be done later!!!!!!!!</h1>{' '}
+      <Button variant="link">
+        <Link href="/dashboard">Dashboard</Link>
+      </Button>
     </div>
   );
 };
