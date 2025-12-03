e47b52df3e9333c4113d5dd3aedffeb04347f583
Yash Raj <kyashraj991@gmail.com>
2025-12-03 16:22:20 +0530

    feat: add SignInButton component for user authentication.

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,6 +1,6 @@
-// components/SignInButton.tsx
 'use client';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { Button } from './ui/button';
 
 export default function SignInButton() {
   const { data: session } = useSession();
@@ -12,8 +12,8 @@ export default function SignInButton() {
     );
   }
   return (
-    <button onClick={() => signIn('google', { callbackUrl: '/dashboard' })}>
+    <Button onClick={() => signIn('google', { callbackUrl: '/dashboard' })}>
       Sign in with Google
-    </button>
+    </Button>
   );
 }
