bc1f619a730e1302e1eb2786eca595ba536f9dab
Yash Raj <kyashraj991@gmail.com>
2025-12-12 22:10:44 +0530

    feat: add try catch

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -2,14 +2,19 @@ import { GoogleGenAI } from '@google/genai';
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export async function Gemini(prompt: string) {
-  const response = await ai.models.generateContent({
-    model: 'gemini-2.5-flash',
-    contents: prompt,
-    config: {
-      systemInstruction:
-        'You prepare complete updated source files for GitHub pull requests. Never output partial code. Never output reasoning.',
-      temperature: 0.2,
-    },
-  });
-  return response;
+  try {
+    const response = await ai.models.generateContent({
+      model: 'gemini-2.5-flash',
+      contents: prompt,
+      config: {
+        systemInstruction:
+          'You prepare complete updated source files for GitHub pull requests. Never output partial code. Never output reasoning.',
+        temperature: 0.2,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error('Error in Gemini:');
+    return null;
+  }
 }
